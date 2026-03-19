0f12e3ff97582f3d3d1ab63bd30700588333615c
Bartosz Pierzchała <b.pierzchala02@gmail.com>
2026-03-19 16:03:50 +0100

    Test commitu

---
+++ db_modelling_gui_2026/db_modelling_gui_2026/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace db_modelling_gui_2026
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
