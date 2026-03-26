a30cbc22e8257ea10bec716a04f0fc6f2125f8d4
Jakub <jakub.nowak2002@op.pl>
2026-03-26 15:40:37 +0100

    projekt ui

--- db_modelling_gui_2026/db_modelling_gui_2026/MainWindow.xaml.cs
+++ db_modelling_gui_2026/db_modelling_gui_2026/MainWindow.xaml.cs
@@ -1,25 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace db_modelling_gui_2026
 {
-    /// <summary>
-    /// Logika interakcji dla klasy Window1.xaml
-    /// </summary>
-    public partial class Window1 : Window
+    public partial class MainWindow : Window
     {
-        public Window1()
+        public MainWindow()
         {
             InitializeComponent();
         }
