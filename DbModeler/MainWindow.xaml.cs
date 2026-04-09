43e3ae1dd9501e7889dd1b61901f8e34ce4b6108
Bartosz <b.pierzchala02@gmail.com>
2026-04-09 14:43:27 +0200

    Szkielet logiki trybu graficznego

--- DbModeler/MainWindow.xaml.cs
+++ DbModeler/MainWindow.xaml.cs
@@ -1,24 +1,56 @@
-﻿using System.Text;
+﻿using DbModeler.Models;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DbModeler
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private bool _isDragging = false;
+        private Point _lastMousePosition;
+        private Table? _draggedTable;
+        private FrameworkElement? _draggedElement;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void TableBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is FrameworkElement element && element.DataContext is Table table)
+            {
+                _isDragging = true;
+                _draggedTable = table;
+                _draggedElement = element;
+                _lastMousePosition = e.GetPosition(this);
+                element.CaptureMouse();
+            }
+        }
+
+        private void TableBlock_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_isDragging && _draggedTable != null && _draggedElement != null)
+            {
+                Point currentMousePosition = e.GetPosition(this);
+                double deltaX = currentMousePosition.X - _lastMousePosition.X;
+                double deltaY = currentMousePosition.Y - _lastMousePosition.Y;
+
+                _draggedTable.CanvasX += deltaX;
+                _draggedTable.CanvasY += deltaY;
+                _lastMousePosition = currentMousePosition;
+            }
+        }
+
+        private void TableBlock_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (_isDragging && _draggedElement != null)
+            {
+                _isDragging = false;
+                _draggedElement.ReleaseMouseCapture();
+                _draggedTable = null;
+                _draggedElement = null;
+            }
         }
     }
 }
